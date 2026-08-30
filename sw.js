7e53dd7a3c6e129a7b3a48a23918367979d0a8b0
Shihaaz Buhary <shihaazbz@gmail.com>
2026-08-30 18:43:40 +0300

    Refine stage path layout

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_VERSION = "quickmaths-20260830-11";
+const CACHE_VERSION = "quickmaths-20260830-12";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./assets/css/styles.css?v=20260830-11",
+  "./assets/css/styles.css?v=20260830-12",
   "./assets/js/version.js",
-  "./assets/js/icons.js?v=20260830-11",
-  "./assets/js/app.js?v=20260830-11",
+  "./assets/js/icons.js?v=20260830-12",
+  "./assets/js/app.js?v=20260830-12",
   "./manifest.webmanifest",
   "./assets/icons/quickmaths-icon.svg",
   "./assets/backgrounds/levels.svg"
